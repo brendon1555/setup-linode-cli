81dfa99f2fb88ac1a944927f600e26df4ab7ffdb
Brendon Lees <brendon1555@gmail.com>
2021-08-01 19:19:26 +1000

    fix: update the way environment variables are set

--- src/setup-python.ts
+++ src/setup-python.ts
@@ -8,7 +8,7 @@ async function run(): Promise<void> {
 
     const matchersPath = path.join(__dirname, '..', '.github')
     const linodeCliToken: string = core.getInput('LINODE_CLI_TOKEN')
-    console.log(`::set-env name=LINODE_CLI_TOKEN::${linodeCliToken}`)
+    core.exportVariable('LINODE_CLI_TOKEN', linodeCliToken)
     console.log(`##[add-matcher]${path.join(matchersPath, 'python.json')}`)
   } catch (err) {
     core.setFailed(err.message)
